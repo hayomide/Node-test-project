f64155cdf9a04bef917ab8fb02ed63d89e298822
Elljay <hayomidebire@gmail.com>
2021-07-07 11:45:21 +0100

    using res.status

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const _ = require("underscore");
+const express = require("express");
+const app = express();
 
-const result = _.contains([1, 2, 3, 4], 3);
-console.log(result);
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given id is not found!");
+  res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
